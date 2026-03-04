91cf59b31492d6247ce71b96dc5b7991d3f158ae
James Day <jimmybobday@gmail.com>
2026-03-04 16:35:46 +1300

    Docker updates

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'standalone',
+  eslint: { ignoreDuringBuilds: true },
+  typescript: { ignoreBuildErrors: true },
   reactCompiler: true,
 };
 
